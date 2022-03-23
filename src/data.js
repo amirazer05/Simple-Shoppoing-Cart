d0bc4267ea6ac4af7009f3a15e6760a041767101
AmirAzer <amirazer555@gmail.com>
2022-03-23 10:18:07 +0200

    create LeanScaleTask

---
+++ src/data.js
@@ -0,0 +1,26 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      code:'R01',
+      name: 'Red Plate',
+      price: 32.95,
+      image: 'https://image.shutterstock.com/image-photo/empty-red-plate-on-white-600w-133090343.jpg',
+    },
+    {
+      id: '2',
+      code:'G01',
+      name: 'Green Plate',
+      price: 24.95,
+      image: 'https://image.shutterstock.com/image-photo/top-viewantique-green-plate-made-600w-1723590985.jpg',
+    },
+    {
+      id: '3',
+      code:'B01',
+      name: 'Blue Plate',
+      price: 7.95,
+      image: 'https://image.shutterstock.com/image-photo/empty-plate-isolated-on-white-260nw-1552862075.jpg',
+    },
+  ],
+};
+export default data;
